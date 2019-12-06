370f07f678aa2bd0bf9a002bf9e891d7c8cbbd18
anderssonderby <anders.01@live.dk>
2019-12-06 13:39:39 +0100

    Unfinished doctor list stuff

--- src/View/DoctorList/DoctorListViewController.java
+++ src/View/DoctorList/DoctorListViewController.java
@@ -2,8 +2,26 @@ package View.DoctorList;
 
 import View.ViewHandler;
 import ViewModel.DoctorList.DoctorListViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class DoctorListViewController {
+
+    @FXML
+    private TableView<Doctor> doctorTableView;
+    @FXML
+    private TableColumn FNameCol;
+    @FXML
+    private TableColumn LNameCol;
+    @FXML
+    private TableColumn OfficeNoCol;
+    @FXML
+    private TableColumn PhoneNoCol;
+    @FXML
+    private TableColumn emailCol;
+
     public void init(DoctorListViewModel patientViewModel, ViewHandler viewHandler) {
+
     }
 }
